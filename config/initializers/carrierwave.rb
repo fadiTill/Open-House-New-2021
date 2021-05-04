7e79a4ef1bc8ff7f66b7313357258b3a0078e7a8
Fadimata Tillman <fadi.tillman@gmail.com>
2021-05-04 13:20:04 -0700

    change

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,20 @@
 # CarrierWave.configure do |config|
 #     config.fog_credentials = {
 #       provider:              'AWS',
-#       aws_access_key_id:     'AKIARTYJZVKHUHAF5GOB',                                      
-#       aws_secret_access_key: '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', 
+#       aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                                      
+#       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], 
 #     }
 #      config.storage = :fog
 #     config.permissions = 0666
 #     config.cache_dir = "#{Rails.root}/tmp/"
-#     config.fog_directory  =   'open-house-new-2021'                                   
+#     config.fog_directory  = ENV['FOG_DIRECTORY']                                   
 #     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
     
 #   end
 
+#documentation
+#   CarrierWave.configure do |config|
+#     config.permissions = 0666
+#     config.directory_permissions = 0777
+#     config.storage = :file
+#   end
