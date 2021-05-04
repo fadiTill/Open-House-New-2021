1c3b0e92c5f79ccbd3a019b4ed86c44866eb59b5
Fadimata Tillman <fadi.tillman@gmail.com>
2021-05-04 13:18:54 -0700

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,13 @@
 # CarrierWave.configure do |config|
 #     config.fog_credentials = {
 #       provider:              'AWS',
-#       aws_access_key_id:     'AKIARTYJZVKHUHAF5GOB',                                      
-#       aws_secret_access_key: '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', 
+#       aws_access_key_id:    ,                                      
+#       aws_secret_access_key: , 
 #     }
 #      config.storage = :fog
 #     config.permissions = 0666
 #     config.cache_dir = "#{Rails.root}/tmp/"
-#     config.fog_directory  =   'open-house-new-2021'                                   
+#     config.fog_directory  =                                    
 #     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
     
 #   end
