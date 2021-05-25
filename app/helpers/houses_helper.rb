1b3575ee5ac2369cecc471adfc0a8810d0f6c327
Fadimata Tillman <fadi.tillman@gmail.com>
2021-05-24 17:43:05 -0700

    links and style

--- app/helpers/houses_helper.rb
+++ app/helpers/houses_helper.rb
@@ -1,6 +1,6 @@
-module HousesHelper
-    def house_photo house
-        img = house.photo.present?? house.photo: "placeholder.png"
-        image_tag house.photo, class:"photo"
-end
-end
+ module HousesHelper
+#     def house_photo house
+#         img = house.photo.present?? house.photo: "placeholder.png"
+#         image_tag img, class:"photo"
+ end
+# end
